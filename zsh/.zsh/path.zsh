6ce29bdbd1e09a5a9dd97184981e158eaef62fc9
Alex Macleod <alex@macleod.io>
2016-02-27 17:41:37 +0000

    Append new path items rather than prepend

--- zsh/.zsh/path.zsh
+++ zsh/.zsh/path.zsh
@@ -1,8 +1,8 @@
 export PATH
 path=(
+  $path
   ~/.gem/ruby/*/bin(Nn[-1])
   ~/.node_modules/bin
-  '/cygdrive/e/Nexus Root Toolkit/data'
-  $path
+  '/cygdrive/e/Android/SDK/platform-tools'
 )
 path=( ${(u)^path:A}(N/) )
