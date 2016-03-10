bf9585bd90f5e77e906634baf61de1eb65082428
Alex Macleod <alex@macleod.io>
2016-03-10 13:01:44 +0000

    Added word diff

--- zsh/.zsh/env.zsh
+++ zsh/.zsh/env.zsh
@@ -2,7 +2,7 @@ export PAGER=vimpager
 export EDITOR=vim
 export TERM=xterm-256color
 
-if [[ $(uname -o) == 'Cygwin' ]]; then
+if [[ $(uname -o) == Cygwin ]]; then
   export GROFF_NO_SGR=1
   export PAGER=less
 fi
--- zsh/.zsh/path.zsh
+++ zsh/.zsh/path.zsh
@@ -3,6 +3,6 @@ path=(
   $path
   ~/.gem/ruby/*/bin(Nn[-1])
   ~/.node_modules/bin
-  '/cygdrive/e/Android/SDK/platform-tools'
+  /cygdrive/c/users/Alex/AppData/Local/Android/sdk/platform-tools
 )
 path=( ${(u)^path:A}(N/) )
