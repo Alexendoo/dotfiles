35d470899e4403314d14158549b35da33f3c386a
Alex Macleod <alex@macleod.io>
2016-04-05 13:18:24 +0100

    Set pager to vimpager if avaiable else less

--- zsh/.zsh/env.zsh
+++ zsh/.zsh/env.zsh
@@ -1,8 +1,12 @@
-export PAGER=vimpager
 export EDITOR=vim
 export TERM=xterm-256color
 
+if which vimpager >/dev/null 2>&1; then
+  export PAGER=vimpager
+else
+  export PAGER=less
+fi
+
 if [[ $(uname -o) == Cygwin ]]; then
   export GROFF_NO_SGR=1
-  export PAGER=less
 fi
