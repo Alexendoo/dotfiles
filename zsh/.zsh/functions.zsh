93988cbdd55e433ffcece78be2bdc9e797f63307
Alex Macleod <alex@macleod.io>
2016-07-18 00:32:43 +0100

    Add tcp listening function

--- zsh/.zsh/functions.zsh
+++ zsh/.zsh/functions.zsh
@@ -1,3 +1,11 @@
 serve () {
   browser-sync start --port $(shuf -i 49152-65535 -n 1) --no-ui --no-notify --server $@
 }
+
+listen () {
+  trap "return 0" INT
+  while true; do
+    nc -l $@ || return 1
+    printf "\x1b[34m――――― %s ―――――\x1b[0m\n" "$(date +"%T.%3N")"
+  done
+}
