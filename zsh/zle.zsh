1d2760288fe9dd648c729f2586add06e21920b0a
Alex Macleod <alex@macleod.io>
2016-08-23 21:50:42 +0100

    add comment line keybind (alt + #)

--- zsh/zle.zsh
+++ zsh/zle.zsh
@@ -3,7 +3,7 @@ autozle() {
   autoload -U $1 && zle -N $1
 }
 
-# enter keypad transmit mode whilst in the zle
+# enter keypad transmit mode whilst in the zle - terminfo(5)
 if (( ${+terminfo[smkx]} && ${+terminfo[rmkx]} )); then
     function zle-line-init () {
         print -n ${terminfo[smkx]}
@@ -18,9 +18,13 @@ fi
 # emacs like key bindings
 bindkey -e
 
-# show only commands matching the current line up to the cursor
+# up/down -- search history using current line
 bindkey "${terminfo[kcuu1]}" up-line-or-search
 bindkey "${terminfo[kcud1]}" down-line-or-search
+
+# shift + right/left -- don't enter junk text into buffer
+bindkey "${terminfo[kRIT]}" forward-char
+bindkey "${terminfo[kLFT]}" backward-char
 
 # shift + tab -- back one position in autocomplete
 zmodload -i zsh/complist
@@ -32,9 +36,17 @@ bindkey "${terminfo[khome]}" beginning-of-line
 bindkey "${terminfo[kend]}" end-of-line
 
 # ctrl + right/left -- move words
-# xterm
 bindkey '\e[1;5C' forward-word
 bindkey '\e[1;5D' backward-word
-# urxvt
-bindkey '\eOc' forward-word
-bindkey '\eOd' backward-word
+
+# ctrl + r -- pattern search history
+bindkey '^R' history-incremental-pattern-search-backward
+
+# alt + # -- comment line and enter (bash muscle memory)
+bindkey '^[#' comment-line
+
+comment-line() {
+  BUFFER="#$BUFFER"
+  zle accept-line
+}
+zle -N comment-line
