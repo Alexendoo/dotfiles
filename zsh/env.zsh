e20a1316c2997fd01bda1e779b3febabeb968b95
Alex Macleod <alex@macleod.io>
2017-06-03 23:45:49 +0100

    add default less args

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -11,6 +11,9 @@ if [[ $(uname -o) == Cygwin ]]; then
   export GROFF_NO_SGR=1
 fi
 
+# less default opts
+export LESS="-x 4 -cR -+FX"
+
 # less colours
 export LESS_TERMCAP_mb=$'\E[01;31m'
 export LESS_TERMCAP_md=$'\E[01;31m'
